30926dca349596c8fe0a13f190ed4b202df2179c
LeeShabani <Ali.shabani6528@gmail.com>
2025-12-29 01:55:56 +0330

    implementaion of Task1-library for practicing oop

---
+++ Task1/Task1-oop-library/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1_oop_library;
+
+
+namespace HomeWork3_Task1
+{
+    public class Bookcs
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Isbn { get; set; }
+        public bool IsAvailable;
+
+        public Bookcs(string title, string author, int isbn)
+        {
+            Title = title;
+            Author = author;
+            Isbn = isbn;
+            IsAvailable = true;
+        }
+    }
+}
